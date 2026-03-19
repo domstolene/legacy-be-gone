e6b5de5c0cb56f42742763588ffe6545283ad005
Arve Seljebu <arve.seljebu@webstep.no>
2026-03-19 10:16:18 +0100

    GLAD-391 fjerne eventuelle kopier som er gjort med tidligere installasjon

--- skills/install.sh
+++ skills/install.sh
@@ -13,6 +13,14 @@ for skill in $skills; do
     skill_folder=$(realpath "$skill")
     target_folder="$HOME/.copilot/skills/$skill_name"
 
+    # if target folder already exists and is not a symbolic link, remove folder and create symbolic link
+    if [ -e "$target_folder" ] && [ ! -L "$target_folder" ]; then
+        echo "Warning: Target folder $target_folder already exists and is not a symbolic link."
+        echo "Removing folder and creating symbolic link for skill: $skill_name"
+        read -rp "Remove? [ctrl-c to cancel] "
+        rm -rf "$target_folder"
+    fi
+
     # check if symolic link already exists, if it does, skip installation
     if [ -L "$target_folder" ]; then
         echo "Symbolic link for skill: $skill_name already exists, skipping installation"
